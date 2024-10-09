e7fe845cb696aa2a630612c187058d6fd3cf3c75
v019-exe <v019.exe@gmail.com>
2024-10-09 20:24:45 +0200

    Restructura del repo

--- Exemple 9/diseño.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE PRODUCTES (
-  id_producte int PRIMARY KEY,
-  nom varchar(50) NOT NULL,
-  descripcio varchar(50) NOT NULL,
-  preu DECIMAL(10,2) NOT NULL,
-  id_componente INT,
-  FOREIGN KEY (id_componente) REFERENCES PRODUCTES(id_producte)
-);
