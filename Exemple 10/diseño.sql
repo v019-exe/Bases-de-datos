318f7c4ab5a909e59580cf108efc14ebb79cc2e3
v019-exe <v019.exe@gmail.com>
2024-10-09 20:23:50 +0200

    Delete Exemple 10 directory

--- Exemple 10/diseño.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE PERSONA (
-  id_persona int PRIMARY KEY,
-  nom varchar(50) NOT NULL,
-  cognoms varchar(50) NOT NULL,
-  data_naixement date NOT NULL,
-  id_amiga int,
-  FOREIGN KEY (id_amiga) REFERENCES PERSONA(id_persona)
-);
